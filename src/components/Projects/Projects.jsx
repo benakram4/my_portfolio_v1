d824cbfd7bf128c4e59be170798c8e9e840ac021
bakram4 <bakram4@myseneca.ca>
2023-12-25 19:31:17 -0500

    trigger update

---
+++ src/components/Projects/Projects.jsx
@@ -0,0 +1,27 @@
+import projects from "./projectsList.js";
+import Project from "@/components/Projects/Project.jsx";
+
+export default function Projects() {
+  return (
+    <div id="projects" className="container my-24 mx-auto md:px-6">
+      <section className="mb-32 text-center">
+        <h2 className="mb-12 pb-4 text-center text-3xl font-bold text-stone-900 dark:text-stone-100">
+          Projects I Am Proud Of
+        </h2>
+        <div className="grid gap-6 lg:grid-cols-3 xl:gap-x-12">
+          {projects.map((project) => (
+            <Project
+              title={project.title}
+              description={project.description}
+              imgSrc={project.image}
+              tags={project.tags}
+              key={project.title}
+              repo={project?.repo}
+              live={project?.live}
+            />
+          ))}
+        </div>
+      </section>
+    </div>
+  );
+}
---
+++ src/components/Projects/projectsList.js
@@ -0,0 +1,41 @@
+const projects = [
+  {
+    title: "Events Ticketing Management",
+    image: "/projects/tickex-project.png",
+    repo: "https://github.com/benakram4/Tickex",
+    live: "https://tickex.vercel.app/",
+    description:
+      "Followed MVC principles to design and build a full-stack web app using Django and PostgreSQL, enabling users to buy, sell, and manage events and venues. Showcased problem-solving skills while implementing the relational database tables. Collaborated with team members and showcased strong communication skills to achieve project goals. login with username: user1 and password: user1user1",
+    tags: ["Django", "PostgreSQL", "MVC", "Bootstrap", "Vercel"],
+  },
+  {
+    title: "Fragments Full Stack App",
+    image: "/projects/aws_project.jpg",
+    description:
+      "Developed a scalable AWS-deployed microservice for handling text and image fragments. The service offers a REST API for CRUD operations and data conversion, with Next.js for front-end interaction. The project, developed on GitHub, includes a CI pipeline with Jest and Hurl for testing.",
+    tags: [
+      "Docker",
+      "AWS Cognito",
+      "Amplify",
+      "ECS",
+      "ECR",
+      "S3",
+      "DynamoDB",
+      "Next.js",
+      "Tailwind",
+      "Jest",
+      "Hurl",
+    ],
+  },
+  {
+    title: "Metropolitan Museum Gallery",
+    image: "/projects/metropolitan-project.png",
+    repo: "https://github.com/benakram4/metropolitan_museum",
+    // live: "https://metropolitanmuseum.vercel.app/",
+    description:
+      "Built a user-friendly Next.js web app for users to browse and save favorite items from the Metropolitan Museum Gallery API. Secured user authentication using bcrypt and stored data in MongoDB. Demonstrated high degree of self-motivation and diligence, independently driving the project to completion ahead of the anticipated timeline.",
+    tags: ["Next.js", "bcrypt", "MongoDB", "Bootstrap", "Vercel"],
+  },
+];
+
+export default projects;
